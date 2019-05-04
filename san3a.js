c0c6b99ef28682ea4f95b3ce231cc7dd93d4bcb9
Khaled AMARI <khaled_amari@live.fr>
2019-05-04 20:19:23 +0200

    validate form ok

--- san3a.js
+++ san3a.js
@@ -1,29 +1,33 @@
 
 //form validation
 
-const validateForm=() =>{
-    let fn = document.form.getElementById("exampleInputName")
-    console.log(fn.innertext)
-    let p = document.getElementById("exampleInputPassword1")
-    let e = document.getElementById("exampleInputEmail1")
-    let regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
-    let strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})")
-    if ((fn.innertext == "")) {
-      alert("please enter your name")
-      return false
-    }
-    if(!strongRegex.test(p)){
-    alert("Invalid Password: The password must be a combination of charatacters , numbers and at least a capital letter")
-    return false }
-
-    if(!regex.test(e)){
-      alert("bad e-mail adress: mail adress must contain '@' and '.'")
-      return false
-    }
-
-    alert('submission succeded')
+function validateForm() {
+  let fn = document.forms["validationk"]["fname"].value;
+  let p = document.forms["validationk"]["Password"].value;
+  let e = document.forms["validationk"]["E-mail"].value;
+  let rep = document.forms["validationk"]["rePassword"].value;
+  let regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+  let strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})")
+  if (fn == "")  {
+    alert("please enter your name")
     return false
   }
 
+  if(!strongRegex.test(p)){
+  alert("Invalid Password")
+  return false; }
+
+  if ((rep) !== p){alert("please enter the same password") }
+
+  if(!regex.test(e)){
+    alert("bad e-mail adress")
+    return false
+  }
+
+  alert('submission succeded')
+  return false
+  
+}
   /*modal*/
   
+
