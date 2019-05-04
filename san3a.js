c2f3abd6487986e91465bb7d0860d41828079898
Khaled AMARI <khaled_amari@live.fr>
2019-05-04 13:02:16 +0200

    ajout du modal singin singup

--- san3a.js
+++ san3a.js
@@ -1,18 +1,20 @@
+
 //form validation
 
-function validateForm() {
-    let fn = document.getElementById('#exampleInputName').value;
-    let p = document.getElementById("#exampleInputPassword1").value;
-    let e = document.getElementById("#exampleInputEmail1").value;
+const validateForm=() =>{
+    let fn = document.form.getElementById("exampleInputName")
+    console.log(fn.innertext)
+    let p = document.getElementById("exampleInputPassword1")
+    let e = document.getElementById("exampleInputEmail1")
     let regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
     let strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})")
-    if ((fn == "")) {
+    if ((fn.innertext == "")) {
       alert("please enter your name")
       return false
     }
     if(!strongRegex.test(p)){
     alert("Invalid Password: The password must be a combination of charatacters , numbers and at least a capital letter")
-    return false; }
+    return false }
 
     if(!regex.test(e)){
       alert("bad e-mail adress: mail adress must contain '@' and '.'")
@@ -22,3 +24,6 @@ function validateForm() {
     alert('submission succeded')
     return false
   }
+
+  /*modal*/
+  
