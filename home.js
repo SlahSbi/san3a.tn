66bb767df55121ced6b3a60fd3275afc88afc5b5
Khaled AMARI <khaled_amari@live.fr>
2019-05-07 15:04:50 +0200

    07/05/2019

--- home.js
+++ home.js
@@ -5,29 +5,31 @@ function validateForm() {
   let fn = document.forms["validationk"]["fname"].value;
   let p = document.forms["validationk"]["Password"].value;
   let e = document.forms["validationk"]["E-mail"].value;
-  let rep = document.forms["validationk"]["rePassword"].value;
+  let te = document.forms["validationk"]["tel"].value;
+  let ad = document.forms["validationk"]["address"].value;
   let regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
   let strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})")
+  let adRegex = new RegExp("^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.{8,})")
+  let teRegex = new RegExp("^(?=.*[0-9])(?=.{8,})")
   if (fn == "")  {
-    alert("please enter your name")
+    alert("SVP entrer votre nom")
     return false
   }
-
+  
+  if(!adRegex.test(ad)){
+    alert("Adresse non valid")
+    return false
+  }
   if(!strongRegex.test(p)){
-  alert("Invalid Password")
+  alert("Le mot de passe doit contenir au moins une lettre, un nombre")
   return false; }
 
-  if ((rep) !== p){
-    alert("please enter the same password")
-return false 
-}
-
   if(!regex.test(e)){
-    alert("bad e-mail adress")
+    alert("Adresse e-mail non valid")
     return false
   }
 
-  window.location='./san3aProfile.html';
+  window.location='./#';
   
 }
 
